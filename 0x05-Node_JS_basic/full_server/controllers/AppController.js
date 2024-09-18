1145d8b95659058368a63e15e668f74b19843b02
Abraham Paul <codexapp44@gmail.com>
2024-09-19 00:51:20 +0100

    task 8 done, full server complete

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,12 @@
+/**
+ * Contains the miscellaneous route handlers.
+ * @author Abraham <https://github.com/zeusmadeit>
+ */
+class AppController {
+    static getHomepage(request, response) {
+      response.status(200).send('Hello Holberton School!');
+    }
+  }
+  
+export default AppController;
+module.exports = AppController;
